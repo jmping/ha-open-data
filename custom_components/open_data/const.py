0519ea2ce6d7fcd5de9766f54c1841ade3946bec
Josh Pasek <josh@joshpasek.com>
2026-07-20 10:25:03 -0400

    Add provider-agnostic Open Data constants

---
+++ custom_components/open_data/const.py
@@ -0,0 +1,22 @@
+"""Constants for the Open Data integration."""
+
+from __future__ import annotations
+
+from homeassistant.const import Platform
+
+DOMAIN = "open_data"
+PLATFORMS = [Platform.SENSOR]
+
+CONF_PROVIDER = "provider"
+CONF_PORTAL_URL = "portal_url"
+CONF_DATASET_ID = "dataset_id"
+CONF_RESOURCE_ID = "resource_id"
+CONF_TIMESTAMP_FIELD = "timestamp_field"
+CONF_SELECTED_FIELDS = "selected_fields"
+
+PROVIDER_SOCRATA = "socrata"
+PROVIDER_CKAN = "ckan"
+PROVIDERS = (PROVIDER_CKAN, PROVIDER_SOCRATA)
+
+DEFAULT_SCAN_INTERVAL_MINUTES = 15
+DEFAULT_TIMESTAMP_FIELD = ":updated_at"
